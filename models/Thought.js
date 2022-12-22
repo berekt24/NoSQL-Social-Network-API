515aa576ee0e023a4cf5ff0b0753fb808fffdfb9
berekt24 <bashenafi35@gmail.com>
2022-12-21 19:53:20 -0500

    added fields, reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -3,7 +3,52 @@ const { Schema, model, Types } = require('mongoose');
 // Schema to create Thought model
 const postSchema = new Schema(
   {
-   
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+      toJSON: {
+          getters: true
+      }
   });
 
 // Initialize our Thought model
