afcd173d2356ae82fbb1d5f6f6ce85df2729cd9a
berekt24 <bashenafi35@gmail.com>
2022-12-21 19:30:21 -0500

    added fields and schema

--- models/User.js
+++ models/User.js
@@ -3,7 +3,44 @@ const { Schema, model } = require('mongoose');
 // Schema to create User model
 const userSchema = new Schema(
   {
-   
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+          validator: function (v) {
+              return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+          },
+          message: props => `${props.value} isn't an email!`
+        }
+      },
+      thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }
+    ],
+    friends: [
+      {
+          type: Schema.Types.ObjectId,
+          ref: 'User'
+      }
+  ]
+},
+{
+  toJSON: {
+      virtuals: true
+  }
+}
+);
+UserSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
   });
 
 // Initialize our User model
