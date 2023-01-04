325f6829098c08eee89faceca3a925d0b8323cc8
berekt24 <bashenafi35@gmail.com>
2023-01-03 20:55:27 -0500

    update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const { User, Thought } = require('../models');
 
 module.exports = {
   getUsers(req, res) {
-    User.find() 
+    User.find()
       .select('-__v')
       .populate('thoughts')
       .populate('friends')
@@ -27,22 +27,36 @@ module.exports = {
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
-
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found at this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  
   addFriend(req, res) {
-   User.findOneAndUpdate(
+    User.findOneAndUpdate(
       { _id: req.params.userId },
       { $addToSet: { friends: req.params.friendId } },
       { new: true }
     ).then((user) =>
-    !user
-      ? res.status(404).json({
+      !user
+        ? res.status(404).json({
           message: ' Found no user with that ID',
         })
-      : res.json('Friend added🎉')
-  )
-  .catch((err) => res.status(500).json(err));
-},
-  
+        : res.json('Friend added🎉')
+    )
+      .catch((err) => res.status(500).json(err));
+  },
+
 };
 
 
